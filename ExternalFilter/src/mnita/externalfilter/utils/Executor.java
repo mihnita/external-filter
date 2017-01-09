921d9b3c5b051de189fffc9d3d1714dc667393fa
Mihai Nita <nmihai_2000@yahoo.com>
2017-01-09 01:23:04 -0800

    Replace my executor with the Apache commons one

--- ExternalFilter/src/mnita/externalfilter/utils/Executor.java
+++ ExternalFilter/src/mnita/externalfilter/utils/Executor.java
@@ -1,106 +1,55 @@
 package mnita.externalfilter.utils;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.Map;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.PumpStreamHandler;
 
 /* TODO: improve executor
  * - expand variables in command line (paths, home folder, environment (see Eclipse, VS itself)
  * - use the script field, maybe even recognize #!
  * - specify code page for input/output files
  * - new lines
- * - erorr reporting (colored output, message box instead of console)
+ * - error reporting (colored output, message box instead of console)
  */
-class ProcessResultReader extends Thread {
-    final InputStream   is;
-    final StringBuilder sb = new StringBuilder();
-    String exceptionMsg = null;
-
-    ProcessResultReader(final InputStream is) {
-        this.is = is;
-    }
-
-    @Override
-    public void run() {
-        try {
-            final InputStreamReader isr = new InputStreamReader(is, "utf-8");
-            final BufferedReader br = new BufferedReader(isr);
-            String line = br.readLine();
-            while (null != line) {
-                sb.append(line).append("\n");
-                line = br.readLine();
-            }
-        } catch (final IOException ioe) {
-            exceptionMsg = ioe.getMessage();
-        }
-    }
-
-    public String getOutputString() {
-        return sb.toString();
-    }
-}
-
-class ProcessInputWriter extends Thread {
-    final OutputStream os;
-    final String[]     stdIn;
-    String exceptionMsg = null;
-
-    ProcessInputWriter(final OutputStream is, String inputText) {
-        this.os = is;
-        // TODO Should fix all this \n stuff in input and output, pass things around untouched
-        if (null != inputText)
-            stdIn = inputText.split("\n");
-        else
-            stdIn = new String[]{};
-    }
-
-    @Override
-    public void run() {
-        try {
-            final OutputStreamWriter osr = new OutputStreamWriter(os, "utf-8");
-            for (String line : stdIn)
-                osr.write(line + "\n");
-            osr.close();
-            os.close();
-        } catch (final IOException e) {
-            exceptionMsg = e.getMessage();
-        }
-    }
-}
-
 public class Executor {
-    public static void execute(ExternalCommand cmd) {
+	private final static String CHARSET = "utf-8"; 
+
+    public static void execute(final ExternalCommand cmd) {
         try {
-            final Process p = Runtime.getRuntime().exec(cmd.getCommandLine());
-            final ProcessResultReader stderr = new ProcessResultReader(p.getErrorStream());
-            final ProcessResultReader stdout = new ProcessResultReader(p.getInputStream());
-            final ProcessInputWriter stdin = new ProcessInputWriter(p.getOutputStream(), cmd.stdIn);
-            stderr.start();
-            stdout.start();
-            stdin.start();
+            final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            final ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
 
-            cmd.exitCode = p.waitFor();
+            final CommandLine commandline = CommandLine.parse(cmd.getCommandLine());
+            final DefaultExecutor exec = new DefaultExecutor();
+            
+            PumpStreamHandler streamHandler; 
+            final String stdIn = cmd.getStdIn();
+            if (stdIn == null) {
+                streamHandler = new PumpStreamHandler(outputStream, errorStream);
+            } else {
+                final ByteArrayInputStream inputStream = new ByteArrayInputStream(stdIn.getBytes(CHARSET));
+                streamHandler = new PumpStreamHandler(outputStream, errorStream, inputStream);
+            }
+            exec.setStreamHandler(streamHandler);
 
-            /* Collect the various results. We assume (for now) that stdout is the right thing,
-             * and everything in stderr is an error.
-             * This is not true for some cases (for instance some applications send the help to stderr
-             * The launcher should be able to specify that, but not in version 1.0
-            */
-            cmd.stdOut = stdout.getOutputString();
-            cmd.stdErr = stderr.getOutputString();
-
-            if (null != stdin.exceptionMsg)
-                cmd.exceptions.add(stdin.exceptionMsg);
-            if (null != stdout.exceptionMsg)
-                cmd.exceptions.add(stdout.exceptionMsg);
-            if (null != stderr.exceptionMsg)
-                cmd.exceptions.add(stderr.exceptionMsg);
-        } catch (final IOException e) {
+            final Map<String, String> environment = System.getenv();
+            cmd.exitCode = exec.execute(commandline, environment);
+            cmd.stdOut = outputStream.toString(CHARSET);
+            cmd.stdErr = errorStream.toString(CHARSET);
+        } catch (final ExecuteException e) {
             cmd.exceptions.add(e.getMessage());
-        } catch (final InterruptedException e) {
+        } catch (final IOException e) {
             cmd.exceptions.add(e.getMessage());
         }
     }
